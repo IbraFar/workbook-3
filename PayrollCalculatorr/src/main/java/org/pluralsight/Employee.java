bed822eb4659fa6c7b2efc4b8a87a729292c3c07
AbdullahA06 <aabdullaev0283@bths.edu>
2023-10-19 10:54:32 -0400

    initial commit

---
+++ PayrollCalculatorr/src/main/java/org/pluralsight/Employee.java
@@ -0,0 +1,39 @@
+package org.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    public double getPayRate() {
+        return payRate;
+    }
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+    @Override
+    public String toString() {
+        return String.format("Employee ID: %d, Name: %s, Gross Pay: %.2f", getEmployeeId(), getName(), getGrossPay());
+    }
+}
